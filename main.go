15253e887a0f1895573d14bcf4b6cb51632ca064
Krowemoh <krowemoh@hotmail.com>
2024-02-21 00:49:57 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/websocket"
+)
+
+type Message struct {
+    Room string `json:"room"`
+    Type string `json:"type"`
+    Username string `json:"username"`
+    Message string `json:"message"`
+}
+
+type Client struct {
+    Room string
+    Username string
+}
+
+var clients = make(map[*websocket.Conn]Client)
+var processMessage = make(chan Message)
+
+var upgrader = websocket.Upgrader {
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool { 
+        return true 
+    },
+}
+
+func wsConnections(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer conn.Close();
+
+    var msg Message
+    err = conn.ReadJSON(&msg)
+
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    if msg.Type == "join" {
+        clients[conn] = Client {
+            Room: msg.Room,
+            Username: msg.Username,
+        }
+    }
+
+    for {
+        err = conn.ReadJSON(&msg)
+        if err != nil {
+            fmt.Println(err)
+            fmt.Println(msg)
+            delete(clients, conn)
+            return
+        }
+
+        processMessage <- msg
+    }
+}
+
+func processWsMessages() {
+    for {
+        msg := <-processMessage
+
+        for conn, client := range clients {
+            if client.Room == msg.Room {
+                err := conn.WriteJSON(msg)
+                if err != nil {
+                    fmt.Println(err)
+                    conn.Close()
+                    delete(clients, conn)
+                }
+            }
+        }
+    }
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "static/chat.html")
+}
+
+func main() {
+    var port = ":8082"
+
+    http.HandleFunc("/",homePage)
+    http.HandleFunc("/ws",wsConnections)
+
+    go processWsMessages()
+
+    fmt.Println("Started server on: " + port)
+    err := http.ListenAndServe(port,nil)
+
+    if err != nil {
+        panic("Error starting server: " + err.Error())
+    }
+}
